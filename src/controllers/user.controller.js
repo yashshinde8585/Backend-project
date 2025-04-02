668f8687aa4ec3dc06d574c2e0093bd5e8ee9841
yashshinde8585 <yashshinde8585@gmail.com>
2025-04-02 11:49:44 +0530

    user api created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,9 @@
-import asyncHandler from '../routes/asyncHandler.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
 
 const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({
-        message: "Ok"
+        message: "First api working!"
     })
-
-
 })
-
 
 export { registerUser }
